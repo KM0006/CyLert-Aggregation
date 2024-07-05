986acbaf826b6400ad0325e06955534faba22603
Karam Moussa <karammoussa28@gmail.com>
2024-07-06 00:09:48 +0300

    Optimizing Libs

--- CyLertPackage/Utils/OnCallLibs.py
+++ CyLertPackage/Utils/OnCallLibs.py
@@ -1,7 +1,9 @@
 
 import importlib
 
-JsonLib = importlib.import_module("json")
 OsLib = importlib.import_module("os")
+JsonLib = importlib.import_module("json")
 TorchLib =  importlib.import_module("torch")
+KerasLib =  importlib.import_module("keras")
+TensorFlowLib = importlib.import_module("json")
 
